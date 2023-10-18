b06e7ac7f69fba5f69b45c90a8e603f9e5bbd58f
berg45 <pereiraberg62@gmail.com>
2023-10-18 15:17:33 -0300

    inicializando projetos js

---
+++ js/operadores_logicos.js
@@ -0,0 +1,13 @@
+var a, b, eMaior, eDiferente, eIgual
+
+a = 2
+b = 10
+
+eMaior = a > b
+eIgual = a === b
+eDiferente = a !== b
+
+console.log('É Maior ', eMaior)
+console.log('É Igual ', eIgual)
+console.log('É Diferente ', eDiferente)
+
