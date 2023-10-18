4ec3e7503332d96d21923f80826f527a8f4fdde2
berg45 <pereiraberg62@gmail.com>
2023-10-18 15:58:41 -0300

    js

---
+++ js/manipular_textos.js
@@ -0,0 +1,15 @@
+var message, email
+
+message = 'estou estudanado javascript'
+console.log(message.substring(0,9))
+console.log('tamanho da mensagem: ',message.length)
+
+email = 'renatocosta2023@gmail.com'
+
+console.log(email.susbstring(0, email.indexof('@')).tolowercase())
+//console.log(email.substring(email.indesof('@)))
+//console.log('BUSCAR CARACTER PELO INDICE', email.charAt(15))
+
+var nomePessoa = 'ANA PAULA'
+
+console.log('nome pessoa', nomePessoa.tolowercase())
