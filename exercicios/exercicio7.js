60ebdb266cad8b91d4d77ce6d8e06f547fd512a1
berg45 <pereiraberg62@gmail.com>
2023-10-23 17:31:31 -0300

    exercicios,js

--- exercicios/exercicio7.js
+++ exercicios/exercicio7.js
@@ -9,4 +9,4 @@ velocidade = parseFloat(input.question('informe a sua velocidade: ')) / 3.6
 
 tempo = distancia / velocidade
 //regra
-console.log(`Na velocidade de ${velocidade} m/s levara o tempo de ${tempo} s`)
+console.log(`Na velocidade de ${velocidade.toFixed(2)} m/s levara o tempo de ${tempo / 3600} h`)
