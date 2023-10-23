ae6123d58730d05515960f563dec84c63d152622
berg45 <pereiraberg62@gmail.com>
2023-10-23 15:32:29 -0300

    exercicios.js

--- exercicios/exercicio4.js
+++ exercicios/exercicio4.js
@@ -1,12 +1,9 @@
-var nota1, nota2, nota3, media
-
-nome = michael
-nota1 = 6
-nota2 = 8
-nota3 = 9
+var nota1 = 6.5
+var nota2 = 7.5
+var nota3 = 9
 
 media = (nota1 + nota2 + nota3) / 3
 
 if(media >= 7) {
-    console.log(`O aluno ${nome} esta aprovado com a média ${media}`)
+    console.log(`A média ${media}`)
 }
