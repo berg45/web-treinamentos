18af7dffd3f8913a66fb7deed4c264f5989f1ace
berg45 <pereiraberg62@gmail.com>
2023-10-23 13:56:02 -0300

    meus exercicios

---
+++ exercicios/exercicio3.js
@@ -0,0 +1,10 @@
+const input = require('readline-sync')
+var numA, numB
+
+numA = 8
+numB = 15
+
+var numA = input.question('informe o primeiro numero?')
+var numB = input.question('informe o segundo numero?')
+
+console.log(`olá, ${numB} ${numA}`)
